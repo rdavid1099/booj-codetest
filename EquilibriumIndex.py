e6a23409e9d0ad437613d1ad64f1e792ff936c88
Ryan Workman <rdavid1099@gmail.com>
2017-02-28 11:28:26 -0700

    Equilibrium index completed

--- EquilibriumIndex.py
+++ EquilibriumIndex.py
@@ -1,1 +1,19 @@
 def eqindex(data):
+    # Initially I thought about solving the algorithm iterating through the array
+    # and using two variables (left = 0  and right = sum(data)) to be maniuplated.
+    # The initial solution was working, but it was taking into account the value
+    # of the number at that particular index.
+    # For example, I know the first solution should return 3 and 6... I was getting
+    # just 5 because including data[5] (which is 3) created 0 == 0 and it returned
+    # the index.
+    i = 0
+    result = []
+    while i < len(data):
+        # After realizing I was NOT supposed to include the value of the current
+        # index, I created this simple boolean to determine the sum of the two
+        # sides properly.
+        if sum(data[:i]) == sum(data[i+1:]):
+            result.append(i)
+        i += 1
+    return result
+    # If there is no solution... I return an empty array.
