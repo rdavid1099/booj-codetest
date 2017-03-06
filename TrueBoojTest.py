4ea2fed0026a35f59c087fae58c45d57557cf90a
Ryan Workman <rdavid1099@gmail.com>
2017-03-06 11:46:42 -0700

    Remove extraneous code in TrueBoojTest

--- TrueBoojTest.py
+++ TrueBoojTest.py
@@ -1,33 +1,3 @@
 from TrueBooj import truebooj
 
 print "\n".join(truebooj(x) for x in xrange(1, 75))
-1
-2
-Fizz
-4
-Buzz
-Fizz
-7
-8
-Fizz
-Buzz
-11
-Fizz
-13
-14
-Fizz Buzz
-16
-17
-Fizz
-19
-Buzz
-Fizz
-22
-23
-Fizz
-Buzz
-26
-Fizz
-28
-29
-Fizz Buzz
